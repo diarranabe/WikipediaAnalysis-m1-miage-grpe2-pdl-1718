3edaa6cc7a3c9151eb18da8fe7c0917f889089e7
Nabé DIARRASSOUBA <diarranabe@gmail.com>
2017-12-16 22:04:45 +0100

    Test pour featuresFrequencies et ProductFrequencies

--- src/test/java/org/opencompare/PCMManagerTest.java
+++ src/test/java/org/opencompare/PCMManagerTest.java
@@ -1,8 +1,11 @@
 package org.opencompare;
 
 import org.junit.Test;
+import org.opencompare.api.java.PCM;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
@@ -10,33 +13,46 @@ import static org.junit.Assert.assertEquals;
 public class PCMManagerTest {
 
 	@Test
-	public void mostFrequentFeaturesTest() {
-		PCMManager manager = new PCMManager("pcms2/");
-        Map<String, Long> mostFrenquents = Tools.featuresFrequencies(manager.pcmList);
-
+    public void productsFrequenciesTest() {
+        PCMManager manager = new PCMManager("pcms2/");
+        Map<String, Long> mostFrenquents = Tools.productsFrequencies(Tools.conformsPCM(manager.pcmList));
         Iterator it = mostFrenquents.entrySet().iterator();
-		 long occurences = 0;
-			while (it.hasNext()) {
-				Map.Entry pair = (Map.Entry) it.next();
-				occurences += (long)pair.getValue();
-                System.out.println(Tools.featuresFrequencies(manager.pcmList));
-            }
-        assertEquals(occurences, Tools.featuresFrequencies(manager.pcmList).size());
+        int occurences = 0;
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            occurences += (long) pair.getValue();
+        }
+        int nbDeProd = 0;
+        for (PCM p : Tools.conformsPCM(manager.pcmList)) {
+            nbDeProd += p.getProducts().size();
+        }
+        assertEquals(occurences, nbDeProd);
     }
-	
+
+
 	@Test
-	public void mostFrequentProduitTest() {
-		PCMManager manager = new PCMManager("pcms2/");
-        Map<String, Long> mostFrenquents = Tools.productsFrequencies(manager.pcmList);
+    public void featuresFrenquenciesTest() {
+        PCMManager manager = new PCMManager("pcms2/");
+        List<PCM> list = manager.pcmList;
+        List<PCM> list1 = new ArrayList<>();
+        list1.add(list.get(0));
+        list1.add(list.get(0));
+
+        Map<String, Long> mostFrenquents = Tools.featuresFrequencies(Tools.conformsPCM(list1));
 
         Iterator it = mostFrenquents.entrySet().iterator();
-		 long occurences = 0;
-			while (it.hasNext()) {
-				Map.Entry pair = (Map.Entry) it.next();
-				occurences += (long)pair.getValue();
-                System.out.println(Tools.featuresFrequencies(manager.pcmList));
-            }
-		assertEquals(2, 2);
-	}
+        long occurences = 0;
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            occurences += (long) pair.getValue();
+        }
+
+        int nbDeProd = 0;
+        for (PCM p : Tools.conformsPCM(list1)) {
+            nbDeProd += p.getConcreteFeatures().size();
+        }
+        assertEquals(occurences, nbDeProd);
+    }
+
 
 }
